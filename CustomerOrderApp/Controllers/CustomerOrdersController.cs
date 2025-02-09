c5063c436c56e38e00aa7363099575a631d595b7
cansugunduzalp <cansugunduzalp@gmail.com>
2025-02-10 00:52:12 +0300

    Add project files.

---
+++ CustomerOrderApp/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,41 @@
+﻿using CustomerOrderApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace CustomerOrderApp.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        // GET: CustomerOrders
+        public ActionResult Index()
+        {
+            // Örnek bir müşteri oluştur
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com"
+            };
+
+            // Örnek siparişler oluştur
+            var orders = new List<Order>
+            {
+                new Order { Id = 1, ProductName = "Laptop", Price = 1200.00m, Quantity = 1 },
+                new Order { Id = 2, ProductName = "Mouse", Price = 25.00m, Quantity = 2 },
+                new Order { Id = 3, ProductName = "Keyboard", Price = 50.00m, Quantity = 1 }
+            };
+
+            // ViewModel'i oluştur
+            var viewModel = new CustomerOrderViewModel
+            {
+                Customer = customer,
+                Orders = orders
+            };
+
+            // ViewModel'i view'a ilet
+            return View(viewModel);
+        }
+    }
+}
---
+++ CustomerOrderApp/Models/CustomerOrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CustomerOrderApp.Models
+{
+    public class CustomerOrderViewModel
+    {
+        public Customer Customer { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
